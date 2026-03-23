9932debd967d9c108b3ff6292f499bfdb42dc376
Roy van Rijn <roy.van.rijn@gmail.com>
2026-03-23 16:07:39 +0100

    Adding faster experiment, lets see the compression here

--- train_svd2.py
+++ train_svd2.py
@@ -702,12 +702,19 @@ class CausalSelfAttention(nn.Module):
         q = self.c_q(x).reshape(bsz, seqlen, self.num_heads, self.head_dim).transpose(1, 2)
         k = self.c_k(x).reshape(bsz, seqlen, self.num_kv_heads, self.head_dim).transpose(1, 2)
         v = self.c_v(x).reshape(bsz, seqlen, self.num_kv_heads, self.head_dim).transpose(1, 2)
+
         q = F.rms_norm(q, (q.size(-1),))
         k = F.rms_norm(k, (k.size(-1),))
+
         cos, sin = self.rotary(seqlen, x.device, q.dtype)
         q = apply_rotary_emb(q, cos, sin)
         k = apply_rotary_emb(k, cos, sin)
         q = q * self.q_gain.to(dtype=q.dtype)[None, :, None, None]
+
+        q = q.contiguous()
+        k = k.contiguous()
+        v = v.contiguous()
+
         y = F.scaled_dot_product_attention(
             q,
             k,
