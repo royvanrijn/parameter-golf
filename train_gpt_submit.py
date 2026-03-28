f757b58589e6b646e50cad0a180dbfbfe402135a
Roy van Rijn <roy.van.rijn@gmail.com>
2026-03-28 09:51:51 +0100

    leakyrelu

--- train_gpt_submit.py
+++ train_gpt_submit.py
@@ -952,8 +952,9 @@ class MLP(nn.Module):
         self.proj._zero_init = True
 
     def forward(self, x: Tensor) -> Tensor:
-        x = torch.relu(self.fc(x))
-        return self.proj(x.square())
+        return F.leaky_relu(self.fc(x), negative_slope=0.5).square()
+        #x = torch.relu(self.fc(x))
+        #return self.proj(x.square())
 
 
 class Block(nn.Module):
